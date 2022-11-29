5aeabd5f5d41cc7ca91a17eee61e0c474e61c762
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-11-29 01:27:11 +0000

    [update]nagano_cake

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, skip: [:registrations,:passwords] , controllers: {
+  sessions: "admin/sessions"
+}
+
+  root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

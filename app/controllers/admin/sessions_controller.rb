5f005a0c52d95234d84e05a1f0743c7260411fa8
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2022-12-01 12:07:15 +0000

    [update]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,8 +2,6 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  layout 'admin/layouts/application'
-
   protected
   def after_sign_in_path_for(resource)
     admin_root_path
